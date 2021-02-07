92409be064bad71ffce66f5c8824fbc0107a5dc2
Aleksandra Nikolov <nikolov.aleksandra@hotmail.com>
2021-02-08 00:56:22 +0100

    conection

--- client/src/context/alert/AlertState.js
+++ client/src/context/alert/AlertState.js
@@ -1,6 +1,6 @@
 import React, {useReducer} from 'react';
 import AlertContext from './alertContext'
-import alertReducer from './alertContext'
+import alertReducer from './alertReducer'
 import { SET_ALERT, REMOVE_ALERT } from '../types'
 import { v4 as uuidv4 } from 'uuid';
 
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,6 +1,7 @@
 import React, {useReducer} from 'react';
 import AuthContext from './authContext'
-import {authReducer} from './authReducer'
+import axios from 'axios'
+import authReducer from './authReducer'
 import {REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR,
         LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT, CLEAR_ERRORS } from '../types'
 
@@ -16,14 +17,45 @@ import {REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR,
             const [state, dispatch] = useReducer(authReducer, initialState)
 
             // load user
+            const loadUser = () => {
+                console.log('load')
+            }
 
             // register user
+            const register = async formData => {
+                const config = {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+                try {
+                    const res = await axios.post('/api/users', formData, config);
+                    dispatch({
+                        type: REGISTER_SUCCESS,
+                        payload: res.data
+                    })
+                } catch (error) {
+                    dispatch({
+                        type: REGISTER_FAIL,
+                        payload: error.response
+                    })
+                }
+            }
 
             // login user
+            const login = () => {
+                console.log('load')
+            }
 
             // logout
+            const logout = () => {
+                console.log('load')
+            }
 
             // clear errors
+            const clearErrors = () => {
+                console.log('load')
+            }
 
 
             return (
@@ -32,7 +64,12 @@ import {REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR,
                    isAuthenticated: state.isAuthenticated,
                    loading: state.loading,
                    user: state.user,
-                    error: state.error
+                    error: state.error,
+                    register,
+                    loadUser,
+                    login,
+                    logout,
+                    clearErrors
                 }}>
                     {props.children}
                 </AuthContext.Provider>
